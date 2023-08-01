b11902aa648278b604292e07c69a0a80844ef38b
rawarck <rasmushpedersen@msn.com>
2023-08-01 15:32:34 +0200

    first commit

---
+++ middleware.ts
@@ -0,0 +1,10 @@
+export { default } from "next-auth/middleware";
+
+export const config = {
+    mather: [
+        "/trips",
+        "/reservations",
+        "/properties",
+        "/favorites",
+    ]
+}
